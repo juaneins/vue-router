f1159fdefe690b1466aab590e77a2988afb9f83c
juaneins_uio <ballesterosjg@gmail.com>
2023-12-31 12:41:08 -0500

    primeras rutas

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import HomeView from '@/views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      component: import('../views/AboutView.vue')
+    },
+    {
+      path: '/chats',
+      component: import('../views/ChatsView.vue')
+    }
+  ]
+})
+
+export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue';
+
+const username = ref('');
+</script>
+
+<template>
+  <main>
+    <h3>Introduce tu nombre de usuario</h3>
+    <input placeholder="username" v-model="username" />
+    <p>{{ username }}</p>
+  </main>
+</template>
